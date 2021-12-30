cd19f7af0f555f6185359ea11d4b51e491a9c1e5
Leko <leko.noor@gmail.com>
2021-12-30 18:39:15 +0800

    feat: add healthcheck page

---
+++ packages/app/data/loader.ts
@@ -0,0 +1,45 @@
+import { parseFile } from "@fast-csv/parse";
+import fs from "fs/promises";
+import path from "path";
+
+const BASE = path.join(process.cwd(), "..", "..", "archive");
+
+export type Item = {
+  lang: string;
+  owner: string;
+  name: string;
+  starsToday: number;
+  stargazers: number;
+  date: string;
+  url: string;
+};
+
+export async function list(): Promise<Item[][][]> {
+  const dates = await fs.readdir(BASE);
+  return Promise.all(
+    dates.map(async (d) => {
+      const files = await fs.readdir(path.join(BASE, d));
+      return Promise.all(
+        files.map((f): Promise<Item[]> => {
+          return new Promise((resolve, reject) => {
+            const lang = path.basename(f, ".csv");
+            const rows: Item[] = [];
+            parseFile(path.join(BASE, d, f), {
+              headers: true,
+            })
+              .on("data", (chunk) =>
+                rows.push({
+                  ...chunk,
+                  starsToday: parseInt(chunk.starsToday, 10),
+                  stargazers: parseInt(chunk.stargazers, 10),
+                  lang,
+                })
+              )
+              .on("error", reject)
+              .on("end", () => resolve(rows));
+          });
+        })
+      );
+    })
+  );
+}
---
+++ packages/app/pages/health.tsx
@@ -0,0 +1,111 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Item, list } from "../data/loader";
+import styles from "../styles/Health.module.css";
+
+type Props = {
+  posts: Item[][][];
+};
+
+function assertDate(date: Item[][]) {
+  return date.flatMap(assertLanguageItems).find((err) => !!err);
+}
+function assertLanguageItems(lang: Item[]) {
+  return lang.flatMap(assertItem).find((err) => !!err);
+}
+function assertItem(item: Item) {
+  if (!/\d{4}-\d{2}-\d{2}/.test(item.date)) {
+    return `Invalid date: ${JSON.stringify(item.date)}`;
+  }
+  if (!item.lang) {
+    return "`lang` is missing";
+  }
+  if (!item.name) {
+    return "`name` is missing";
+  }
+  if (!item.owner) {
+    return "`owner` is missing";
+  }
+  if (!item.url) {
+    return "`url` is missing";
+  }
+  if (item.stargazers && !isFinite(item.stargazers)) {
+    return `\`stargazers\` is invalid: ${JSON.stringify(item.stargazers)}`;
+  }
+  if (item.starsToday && !isFinite(item.starsToday)) {
+    return "`starsToday` is invalid";
+  }
+}
+
+function AssertionResult({ error }: { error: string | undefined }) {
+  if (error) {
+    console.log({ error });
+    return (
+      <span className={styles.icon} title={error}>
+        ❌
+      </span>
+    );
+  }
+  return <span className={styles.icon}>✅</span>;
+}
+
+const Home: NextPage<Props> = ({ posts }) => {
+  return (
+    <div>
+      <Head>
+        <title>Health check</title>
+        <meta name="robots" content="noindex" />
+      </Head>
+
+      <main className={styles.root}>
+        <h1>Health check</h1>
+        {[...posts].reverse().map((date, i) => (
+          <details className={styles.foldable} key={i} open={i === 0}>
+            <summary>
+              <AssertionResult error={assertDate(date)} />
+              {date.filter((l) => l.length)[0][0]?.date || "-"}
+            </summary>
+            {date
+              .filter((l) => l.length)
+              .map((l, j) => (
+                <details className={styles.foldable} key={l[0]?.lang ?? j}>
+                  <summary>
+                    <AssertionResult error={assertLanguageItems(l)} />
+                    {l[0]?.lang ?? ""} : {l.length}
+                  </summary>
+                  <ul>
+                    {l
+                      .sort((a, b) => b.starsToday - a.starsToday)
+                      .map((item) => (
+                        <li key={`${item.owner}/${item.name}`}>
+                          <AssertionResult error={assertItem(item)} />
+                          <a
+                            href={item.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            (+{item.starsToday ?? "-"}) {item.owner}/{item.name}
+                            ⭐️ {item.stargazers ?? "-"}
+                          </a>
+                        </li>
+                      ))}
+                  </ul>
+                </details>
+              ))}
+          </details>
+        ))}
+      </main>
+    </div>
+  );
+};
+
+export async function getStaticProps(): Promise<{ props: Props }> {
+  const posts = await list();
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Home;
