0d75ef7f5d2ad4f99471a68c5a988e91e601e47c
Leko <leko.noor@gmail.com>
2022-01-01 01:57:46 +0800

    debug

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config) => ({
+    ...config,
+    experiments: {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    },
+  }),
+};
