2cad1d8ce449f6732c5d9943a73c7790a35eb9d1
Leko <leko.noor@gmail.com>
2022-01-01 02:05:14 +0800

    Revert "debug"
    This reverts commit 0d75ef7f5d2ad4f99471a68c5a988e91e601e47c.

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  webpack: (config) => ({
-    ...config,
-    experiments: {
-      ...config.experiments,
-      asyncWebAssembly: true,
-    },
-  }),
-};
+}
