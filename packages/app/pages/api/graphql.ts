1605d9227053bf04d11d2d31b156a5f804bad566
Leko <leko.noor@gmail.com>
2022-01-01 01:52:39 +0800

    debug

--- packages/app/pages/api/graphql.ts
+++ packages/app/pages/api/graphql.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 
 import fs from "fs";
+import path from "path";
 import { createConnection } from "typeorm";
 import { NextApiHandler } from "next";
 import { apolloServer } from "../../src/app";
@@ -8,6 +9,14 @@ import * as entitiesMap from "../../src/entity";
 
 console.log("cwd:", process.cwd());
 console.log("ls:", fs.readdirSync(process.cwd()));
+console.log(
+  "ls node_modules:",
+  fs.readdirSync(path.join(process.cwd(), "node_modules"))
+);
+console.log(
+  "ls node_modules/sql.js:",
+  fs.readdirSync(path.join(process.cwd(), "node_modules", "sql.js"))
+);
 
 export const config = {
   api: {
