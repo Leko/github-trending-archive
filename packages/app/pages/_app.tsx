8bc426e9bef174b1c5a5f5ddf5a325b2765f0249
Leko <leko.noor@gmail.com>
2022-01-02 23:14:34 +0800

    style: tweak spacing

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -1,8 +1,34 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  components: {
+    MuiContainer: {
+      styleOverrides: {
+        root: {
+          paddingRight: "8px",
+          paddingLeft: "8px",
+        },
+      },
+    },
+    MuiAccordionDetails: {
+      styleOverrides: {
+        root: {
+          paddingRight: "8px",
+          paddingLeft: "8px",
+        },
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
