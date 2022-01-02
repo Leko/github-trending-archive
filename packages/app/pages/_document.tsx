0c6b95880a240f3ab6f6f34b5d761b6628b5ba62
Leko <leko.noor@gmail.com>
2022-01-02 21:56:23 +0800

    feat: add og-image API to retrieve OG image from github repository

---
+++ packages/app/pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+function MyDocument() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default MyDocument;
--- packages/app/src/init-db.ts
+++ packages/app/src/init-db.ts
@@ -2,19 +2,22 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { Repository } from "./entity";
 import { list } from "../data/loader";
+import * as ormconfig from "../ormconfig";
 
-Promise.all([createConnection(), list()]).then(async ([connection, ret]) => {
-  const items = ret.flatMap((item) => item.flat());
+Promise.all([createConnection(ormconfig), list()]).then(
+  async ([connection, ret]) => {
+    const items = ret.flatMap((item) => item.flat());
 
-  await Repository.clear();
-  await connection
-    .createQueryBuilder()
-    .insert()
-    .into(Repository)
-    .values(items)
-    .execute();
+    await Repository.clear();
+    await connection
+      .createQueryBuilder()
+      .insert()
+      .into(Repository)
+      .values(items)
+      .execute();
 
-  await Repository.count().then((count) =>
-    console.log(`insert ${count} records`)
-  );
-});
+    await Repository.count().then((count) =>
+      console.log(`insert ${count} records`)
+    );
+  }
+);
